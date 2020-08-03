10237e84a837c46691f31bc93ab0eff6eff451c0
Valentin Chanas <valentin.chanas@dcbrain.com>
2020-08-03 03:17:04 +0200

    absolute paths

--- gatsby-config.js
+++ gatsby-config.js
@@ -52,6 +52,21 @@ module.exports = {
         ],
         display: 'swap'
       }
+    },
+    {
+      resolve: 'gatsby-plugin-module-resolver',
+      options: {
+        root: './src', // <- will be used as a root dir
+        aliases: {
+          components: './components', // <- will become ./src/components
+          elements: './elements'
+          // helpers: './helpers', // <- will become ./src/helpers
+          // static: {
+          //   root: './public', // <- will used as this alias' root dir
+          //   alias: './static' // <- will become ./public/static
+          // }
+        }
+      }
     }
   ]
 }
--- src/components/Container.js
+++ src/components/Container.js
@@ -1,6 +1,6 @@
 import React from 'react'
-import { ContainerWrapper } from '../elements'
-import { Nav } from './Nav'
+import { ContainerWrapper } from 'elements'
+import { Nav } from 'components'
 
 export const Container = ({ children }) => {
   return (
